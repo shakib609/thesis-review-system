056256f79db1aef8d0443b3f260166bf8e999714
Shakib Hossain <shakib609@gmail.com>
2018-05-19 00:41:30 +0600

    Add Login and Logout functionalities for website

--- website/registration/urls.py
+++ website/registration/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import login, logout
 
 from .views import register
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
