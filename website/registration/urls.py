c3f94cf8239dea1a92498217bf3593480db281e2
Shakib Hossain <shakib609@gmail.com>
2018-05-22 03:04:50 +0600

    Redirect Users if they are already logged in

--- website/registration/urls.py
+++ website/registration/urls.py
@@ -6,7 +6,18 @@ from .views import register
 app_name = 'registration'
 
 urlpatterns = [
-    path('', login, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout, {'next_page': '/'}, name='logout'),
+    path(
+        '',
+        login,
+        {'redirect_authenticated_user': True},
+        name='login'),
+    path(
+        'register/',
+        register,
+        name='register'),
+    path(
+        'logout/',
+        logout,
+        {'next_page': '/'},
+        name='logout'),
 ]
