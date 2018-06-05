e80550a4ee68d1dd9ebf4b185e04a57440175b79
Shakib Hossain <shakib609@gmail.com>
2018-06-06 02:18:40 +0600

    Write create_comment view for the app and add js.cookie javascript plugin

--- website/thesis/views.py
+++ website/thesis/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.template.defaultfilters import date
+
+import json
 
 from .forms import (
     StudentGroupForm, StudentGroupJoinForm, DocumentUploadForm)
-from .models import StudentGroup, Document
+from .models import StudentGroup, Document, Comment
 from .decorators import is_student, is_teacher
+
+
+def return_json(data):
+    return HttpResponse(json.dumps(data), content_type='application/json')
 
 
 @login_required
@@ -61,6 +70,28 @@ def group_home(request):
     studentgroup = request.user.studentgroup
     return render(
         request, 'thesis/group_home.html', {'studentgroup': studentgroup})
+
+
+@login_required
+@require_POST
+def create_comment(request, group_code):
+    studentgroup = get_object_or_404(StudentGroup, md5hash=group_code)
+    user = request.user
+    if (user.studentgroup != studentgroup) or user.is_teacher is False:
+        return return_json({'created': False})
+    req = json.loads(request.body.decode('utf-8'))
+    content = req.get('content')
+    comment = Comment(content=content, user=user, studentgroup=studentgroup)
+    try:
+        comment.save()
+        d = comment.created_at
+        response = {
+            'created': True,
+            'created_at': date(d, 'd M Y') + ' at ' + date(d, 'H:i'),
+        }
+        return return_json(response)
+    except:
+        return return_json({'created': False})
 
 
 @login_required
