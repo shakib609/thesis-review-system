8a23e15767f224d8263ea4631298127f331cf7d0
Shakib Hossain <shakib609@gmail.com>
2018-06-06 04:09:46 +0600

    Fix register template and migrate to ClassBasedViews in registration module

--- website/registration/urls.py
+++ website/registration/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import login, logout
 
-from .views import register, about
+from .views import UserCreateView, AboutView
 
 app_name = 'registration'
 
 urlpatterns = [
     path('', login, {'redirect_authenticated_user': True}, name='login'),
-    path('register/', register, name='register'),
+    path('register/', UserCreateView.as_view(), name='register'),
     path('logout/', logout, {'next_page': '/'}, name='logout'),
-    path('about/', about, name='about'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- website/registration/views.py
+++ website/registration/views.py
@@ -1,26 +1,30 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.views.generic import TemplateView, CreateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 from .forms import StudentSignUpForm
+from .models import User
 
 
-def about(request):
-    return render(request, 'registration/about.html')
+class AboutView(TemplateView):
+    template_name = 'registration/about.html'
 
 
-def register(request):
-    redirect_to = 'registration:login'
-    if request.user.is_authenticated:
-        return redirect(redirect_to)
-    if request.method == 'POST':
-        form = StudentSignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect(redirect_to)
-    else:
-        form = StudentSignUpForm()
-    return render(request, 'registration/register.html', {'form': form})
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'registration/register.html'
+    form_class = StudentSignUpForm
+    success_url = reverse_lazy('thesis:account_redirect')
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('registration:login')
+        return super().get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        self.object = user = form.save()
+        request = self.request
+        login(request, user)
+        return HttpResponseRedirect(self.get_success_url())
