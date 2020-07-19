b23d7a31be4bb8f90bbd4c62726c2a459b3db960
Shakib Hossain <shakib609@gmail.com>
2020-07-19 23:55:08 +0600

    Update urls.py to serve static and media files

--- config/urls.py
+++ config/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
     path(
         '',
         include('website.thesis.urls'), name='thesis'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+) + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+)
